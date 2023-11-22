3e563d3d924af2db41ea54bfcded3e2e1bbf6ecf
yangxiongbro <yangxiongbro@163.com>
2023-11-23 00:02:50 +0800

    edit:完善字符串工具类

--- src/main/java/com/common/java/utils/StringsUtils.java
+++ src/main/java/com/common/java/utils/StringsUtils.java
@@ -2,10 +2,14 @@ package com.common.java.utils;
 
 import com.common.java.constants.StringsConstants;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 /**
  * <b><code>StringsUtils</code></b>
  * <p/>
- * <p>
+ * 字符串工具类
  * <p/>
  * <b>Creation Time:</b> 2023/11/21 22:07
  *
@@ -18,26 +22,44 @@ public class StringsUtils {
      * @description: 小驼峰命名法
      * 驼峰命名法是一种常见的变量命名风格，它的特点是将多个单词连接在一起，每个单词的首字母大写，除了第一个单词的首字母小写
      * 例如: firstName、enabled
-     * @param: str - [String]
+     * @param: strArray - [String]
      * @return: java.lang.String
      * @throws
      * @author yang xiong
-     * @date 2023/11/21 22:28
+     * @date 2023/11/22 22:22
      **/
     public static String lowerCamelCase(String[] strArray){
         if(null != strArray && strArray.length > 0){
-            StringBuffer sb = new StringBuffer();
             String str = strArray[0];
             if(isNotEmpty(str)){
-                sb.append(str.toLowerCase());
+                return str.toLowerCase() + upperCamelCase(strArray, 1);
             }
+            return upperCamelCase(strArray, 1);
+        }
+        return StringsConstants.EMPTY_STRING;
+    }
+
+    /**
+     * @description: 大驼峰命名法
+     * 将多个单词连接在一起，每个单词的首字母大写。与小驼峰命名法的区别在于第一个单词的首字母大写
+     * 例如: FirstName、Enabled
+     * @param: strArray - [String]
+     * @param: beginIndex - [int]
+     * @return: java.lang.String
+     * @throws
+     * @author yang xiong
+     * @date 2023/11/22 22:25
+     **/
+    public static String upperCamelCase(String[] strArray, int beginIndex){
+        if(null != strArray && strArray.length > 0 && beginIndex < strArray.length){
             int len = strArray.length;
-            for(int i = 1; i < len; i++){
-                str = strArray[i];
+            StringBuffer sb = new StringBuffer();
+            for(int i = beginIndex; i < len; i++){
+                String str = strArray[i];
                 if(isNotEmpty(str)){
-                    Byte firstChar = StringsConstants.LOWER_UPPER_MAP.get((byte)str.charAt(0));
+                    Byte firstChar = StringsConstants.LOWER_UPPER_CHAR_MAP.get((byte)str.charAt(0));
                     sb.append((char)(null == firstChar ? str.charAt(0) : firstChar));
-                    sb.append(str.substring(1));
+                    sb.append(str.substring(1).toLowerCase());
                 }
             }
             return sb.toString();
@@ -49,60 +71,85 @@ public class StringsUtils {
      * @description: 大驼峰命名法
      * 将多个单词连接在一起，每个单词的首字母大写。与小驼峰命名法的区别在于第一个单词的首字母大写
      * 例如: FirstName、Enabled
-     * @param: str - [String]
+     * @param: strArray - [String]
      * @return: java.lang.String
      * @throws
      * @author yang xiong
      * @date 2023/11/21 22:28
      **/
-    public static String upperCamelCase(String str){
-
-        return str;
+    public static String upperCamelCase(String[] strArray){
+        return upperCamelCase(strArray, 0);
     }
 
     /**
      * @description: 帕斯卡命名法
      * 所有单词的首字母大写，然后直接连接起来，单词之间没有连接符。与大驼峰命名法相同
      * 例如: FirstName、Enabled
-     * @param: str - [String]
+     * @param: strArray - [String]
      * @return: java.lang.String
      * @throws
      * @author yang xiong
      * @date 2023/11/21 22:27
      **/
-    public static String pascalCase(String str){
-
-        return str;
+    public static String pascalCase(String[] strArray){
+        return upperCamelCase(strArray);
     }
 
     /**
      * @description: 下划线命名法
      * 所有字母都小写，单词之间使用下划线连接的命名风格
      * 例如: first_name、enabled
-     * @param: str - [String]
+     * @param: strArray - [String]
      * @return: java.lang.String
      * @throws
      * @author yang xiong
      * @date 2023/11/21 22:27
      **/
-    public static String snakeCase(String str){
-
-        return str;
+    public static String snakeCase(String[] strArray){
+        return connectWord(strArray, StringsConstants.UNDER_LINE).toLowerCase();
     }
 
     /**
      * @description: 全部大写命名法
      * 全部大写命名法是一种将所有字母都大写，单词之间使用下划线连接的命名风格，通常用来表示常量或者全局变量
      * 例如: FIRST_NAME、ENABLED
-     * @param: str - [String]
+     * @param: strArray - [String]
      * @return: java.lang.String
      * @throws
      * @author yang xiong
      * @date 2023/11/21 22:27
      **/
-    public static String allCapsCase(String str){
+    public static String allCapsCase(String[] strArray){
+        return connectWord(strArray, StringsConstants.UNDER_LINE).toUpperCase();
+    }
 
-        return str;
+    /**
+     * @description: 将字符串数组使用连接符号连接起来
+     * @param: strArray - [String]
+     * @param: connector - [String]
+     * @return: java.lang.String
+     * @throws
+     * @author yang xiong
+     * @date 2023/11/22 22:54
+     **/
+    public static String connectWord(String[] strArray, String connector){
+        if(null != strArray && strArray.length > 0 && null != connector){
+            StringBuffer sb = new StringBuffer();
+            int len = strArray.length;
+            String str = strArray[0];
+            if(isNotEmpty(str)){
+                sb.append(str);
+            }
+            for(int i = 1; i < len; i++){
+                str = strArray[i];
+                if(isNotEmpty(str)){
+                    sb.append(connector);
+                    sb.append(str);
+                }
+            }
+            return sb.toString();
+        }
+        return StringsConstants.EMPTY_STRING;
     }
 
     /**
@@ -122,6 +169,11 @@ public class StringsUtils {
 
     /**
      * @description: 将字符串使用分隔符分割为字符串数组
+     * 当limit>0时，limit代表最后的数组长度，同时一共会切割 limit-1 次，最后没有切割完成的直接放在一起
+     * 当limit=0时，会尽量多去匹配，并且会把末尾的空（例如：用逗号分隔后面“name,” 最后就会出现空）去掉
+     * 当limit<0时，会尽量多去匹配，但不会去掉末尾的空
+     * split(regex)调用了split(regex,0)
+     *
      * @param: str - [String]
      * @param: separator - [String]
      * @param: limit - [int]
@@ -137,6 +189,38 @@ public class StringsUtils {
         return StringsConstants.EMPTY_STRING_ARRAY;
     }
 
+    /**
+     * @description: 根据 Set 集合中得 char 队字符串进行分割
+     * @param: str - [String]
+     * @param: charSet - [Byte>]
+     * @return: java.lang.String[]
+     * @throws 
+     * @author yang xiong
+     * @date 2023/11/22 23:54
+     **/
+    public static String[] split(String str, Set<Byte> charSet){
+        if(isNotEmpty(str) && null != charSet && charSet.size() > 0){
+            List<String> list = new ArrayList<>();
+            StringBuffer sb = new StringBuffer();
+            byte[] byteArray = str.getBytes();
+            for(byte b: byteArray){
+                if(charSet.contains(b)){
+                    if(sb.length()>0){
+                        list.add(sb.toString());
+                    }
+                    sb.delete(0, sb.length());
+                }
+                sb.append((char)b);
+            }
+            if(sb.length()>0){
+                list.add(sb.toString());
+            }
+            String[] strArray = new String[list.size()];
+            return list.toArray(strArray);
+        }
+        return StringsConstants.EMPTY_STRING_ARRAY;
+    }
+
     public static boolean isNotEmpty(CharSequence str) {
         return str != null && str.length() > 0 && containsText(str);
     }
@@ -145,7 +229,7 @@ public class StringsUtils {
         return str != null && !str.isEmpty() && containsText(str);
     }
 
-    private static boolean containsText(CharSequence str) {
+    public static boolean containsText(CharSequence str) {
         int strLen = str.length();
 
         for(int i = 0; i < strLen; ++i) {
--- src/test/java/com/common/java/utils/StringsUtilsTest.java
+++ src/test/java/com/common/java/utils/StringsUtilsTest.java
@@ -1,5 +1,6 @@
 package com.common.java.utils;
 
+import com.common.java.constants.StringsConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -16,9 +17,85 @@ import org.junit.jupiter.api.Test;
 @Slf4j
 public class StringsUtilsTest {
 
+    private String str2DArray[][] = new String[][]{
+            new String[]{"first", "name"},
+            new String[]{"FIRST", "NAME"},
+            new String[]{"first", "NAME"},
+            new String[]{"FIRST", "name"},
+            new String[]{"First", "name"},
+            new String[]{"first", "Name"},
+            new String[]{"", "name"},
+            new String[]{null, "name"},
+            new String[]{"first", null},
+            new String[]{null, null},
+            new String[]{},
+            null
+    };
+
+    private String strArray[] = new String[]{null, "", "  ", " first", " first ", "first name", " first name", " first name ", "name"
+            , " First",  "firsT ", " FirsT ", "First Name", " First name", " First namE ", "NAME"};
+
     @Test
     public void lowerCamelCase(){
-        String str[] = new String[]{"first", "name"};
-        log.info("{}", StringsUtils.lowerCamelCase(str));
+        for(String[] strArray:str2DArray){
+            log.info("{} -> {}", strArray, StringsUtils.lowerCamelCase(strArray));
+        }
     }
+
+    @Test
+    public void upperCamelCase(){
+        for(String[] strArray:str2DArray){
+            log.info("{} -> {}", strArray, StringsUtils.upperCamelCase(strArray));
+        }
+    }
+
+    @Test
+    public void pascalCase(){
+        for(String[] strArray:str2DArray){
+            log.info("{} -> {}", strArray, StringsUtils.pascalCase(strArray));
+        }
+    }
+
+    @Test
+    public void snakeCase(){
+        for(String[] strArray:str2DArray){
+            log.info("{} -> {}", strArray, StringsUtils.snakeCase(strArray));
+        }
+    }
+
+    @Test
+    public void allCapsCase(){
+        for(String[] strArray:str2DArray){
+            log.info("{} -> {}", strArray, StringsUtils.allCapsCase(strArray));
+        }
+    }
+
+    @Test
+    public void split() {
+        for(String str:strArray){
+            log.info("{} -> {}", str, StringsUtils.split(str, " ", -1));
+        }
+    }
+
+    @Test
+    public void split2() {
+        for(String str:strArray){
+            log.info("{} -> {}", str, StringsUtils.split(str, StringsConstants.UPPER_CHAR_SET));
+        }
+    }
+
+    @Test
+    public void isNotEmpty() {
+        for(String str:strArray){
+            log.info("{} -> {}", str, StringsUtils.isNotEmpty(str));
+        }
+    }
+
+    @Test
+    public void containsText() {
+        for(String str:strArray){
+            log.info("{} -> {}", str, StringsUtils.containsText(str));
+        }
+    }
+
 }
